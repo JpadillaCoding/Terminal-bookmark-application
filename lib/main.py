3841441c4100f3948cf8bc5e3f8d3cf0c2c40a1e
Jose Padilla <jose.apadilla99@gmail.com>
2023-04-15 15:04:49 -0500

    search and search by title works

--- lib/main.py
+++ lib/main.py
@@ -23,6 +23,7 @@ class Bookmark(BaseModel):
 
 db.create_tables([Bookmark])
 
+
 def home_page():
     print("\n" + "-"*50)
     print("Welcome to your Bookmark collector!")
@@ -32,7 +33,8 @@ def home_page():
     homepage_response = None
 
     while homepage_response not in [1, 2]:
-        homepage_response = int(input("Type in the page you'd like to navigate to: "))
+        homepage_response = int(
+            input("Type in the page you'd like to navigate to: "))
 
         if homepage_response == 1:
             create_bookmark()
@@ -46,12 +48,44 @@ def create_bookmark():
     url = input("Type the bookmark URL: ")
     description = input("Type a brief description of your bookmark: ")
 
-    bookmark = Bookmark( title=title, url=url, description=description )
+    bookmark = Bookmark(title=title, url=url, description=description)
     bookmark.save()
+    print(f"{title} has been saved!")
+    home_page()
 
 
 def bookmarks():
-    print("bookmarks page")
+    print("Bookmarks page: ")
+    print("#1 Show all bookmarks \n#2 Search by name ")
+    bookmark_response = None
+
+    while bookmark_response not in [1, 2]:
+        bookmark_response = int(
+            input("Type in the page you'd like to navigate to: "))
+        print("="*50)
+        print("="*50)
+
+        if bookmark_response == 1:
+            bookmarks = Bookmark.select()
+            for bookmark in bookmarks:
+                print(
+                    f"Title: {bookmark.title}: \nURL: {bookmark.url} \nDescription: {bookmark.description}")
+                print("\n" + "="*50)
+            home_page()
+        elif bookmark_response == 2:
+            search_by_title()
+
+
+def search_by_title():
+    search_title = input("What is the title of the bookmark? ")
+    bookmark = Bookmark.get(Bookmark.title == search_title)
+    print(
+        f"Title: {bookmark.title} \nURL: {bookmark.url} \nDescription: {bookmark.description}")
+    anotherSearch = input("Would you like to make another Search? (y/n) ")
+    if anotherSearch == 'y':
+        search_by_title()
+    else:
+        home_page()
 
 
 home_page()
