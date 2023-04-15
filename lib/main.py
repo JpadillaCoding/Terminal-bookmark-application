861659b9c03cef00e28a42c04fe24ee0415eb0ea
Jose Padilla <jose.apadilla99@gmail.com>
2023-04-15 14:26:44 -0500

    create now works

--- lib/main.py
+++ lib/main.py
@@ -1,5 +1,28 @@
 from peewee import *
 
+db = PostgresqlDatabase(
+    'bookmark',
+    user='postgres',
+    password='',
+    host='localhost',
+    port='5432'
+)
+db.connect()
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Bookmark(BaseModel):
+    title = CharField()
+    url = CharField()
+    description = CharField()
+
+
+db.create_tables([Bookmark])
+
 def home_page():
     print("\n" + "-"*50)
     print("Welcome to your Bookmark collector!")
@@ -9,8 +32,7 @@ def home_page():
     homepage_response = None
 
     while homepage_response not in [1, 2]:
-        homepage_response = int(
-            input("Type in the page you'd like to navigate to: "))
+        homepage_response = int(input("Type in the page you'd like to navigate to: "))
 
         if homepage_response == 1:
             create_bookmark()
@@ -18,12 +40,18 @@ def home_page():
             bookmarks()
 
 
+def create_bookmark():
+    print("Create Bookmarks Here")
+    title = input("Type your bookmark title: ")
+    url = input("Type the bookmark URL: ")
+    description = input("Type a brief description of your bookmark: ")
+
+    bookmark = Bookmark( title=title, url=url, description=description )
+    bookmark.save()
+
+
 def bookmarks():
     print("bookmarks page")
-
-
-def create_bookmark():
-    print("create page")
 
 
 home_page()
