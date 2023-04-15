3e79b4f54168fbf8d346170455bacb5b5feb4e56
Jose Padilla <jose.apadilla99@gmail.com>
2023-04-15 13:54:08 -0500

    hompage function now works

--- lib/main.py
+++ lib/main.py
@@ -1,24 +1,29 @@
 from peewee import *
 
+
 def home_page():
+
     print("Welcome to your Bookmark collector!")
     print("Homepage navigations: \n#1 Create a new Bookmark \n#2 Go to your Bookmark collection")
-    homepage_response = int(input("Type the number of which page you'd like to navigate"))
 
-    def home_page_switch(response):
-        if response == 1:
+    homepage_response = None
+
+    while homepage_response not in [1, 2]:
+        homepage_response = int(
+            input("Type in the page you'd like to navigate to"))
+
+        if homepage_response == 1:
             create_bookmark()
-        elif response == 2:
+        elif homepage_response == 2:
             bookmarks()
-        else:
-            
-        
-    
+
+
 def bookmarks():
     print("bookmarks page")
+
 
 def create_bookmark():
     print("create page")
 
-home_page()
 
+home_page()
