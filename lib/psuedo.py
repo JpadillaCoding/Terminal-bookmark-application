907a26f612c5d9f97581bbd0c2448a446fd9b52c
Jose Padilla <jose.apadilla99@gmail.com>
2023-04-15 13:02:14 -0500

    set up files and psuedo

---
+++ lib/psuedo.py
@@ -0,0 +1,23 @@
+#function()welcome page, show the user the options
+    #use input() to ask the user where they would like to go, using 1-10? as options
+    #1- create new bookmark (priority 1)
+    #2- delete bookmark (priority 3)
+    #3- update bookmark (priority 4)
+    #4- shows the bookmarks in a list (priority 2)
+#db- bookmarks:
+    #bookmarks.name
+    #bookmarks.url
+    #bookmarks.info
+# function(show bookmark item #4)- 
+    #1 show all
+    #User.select( User.name, User.url, User.info )
+    #2 find by name
+    #User.select( User.name )
+# function(create#1)-
+    #1 create new item with a name, the url, and details
+    #return on console the info they put in
+
+# function(delete#2)-
+    #find by name
+    #delete
+
